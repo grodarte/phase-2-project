52d398365d5e82567091e2f0bfe263740db9b0ec
Gabrielle <ms.grodarte@gmail.com>
2024-11-27 15:36:53 -0800

    initial context

--- src/components/Gifts.js
+++ src/components/Gifts.js
@@ -1,9 +1,12 @@
 import { useOutletContext } from "react-router-dom"
+import Gift from "./Gift"
 
 function Gifts(){
     const gifts = useOutletContext()
 
     console.log(gifts)
+
+    const giftElements = gifts.map(gift=><Gift key={gift.id} gift={gift}/>)
 
     return (
         <main>
