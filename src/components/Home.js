c1c57459041e93b994c622b954194c610a86c85f
Gabrielle <ms.grodarte@gmail.com>
2024-12-04 14:43:10 -0800

    initial home website description

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,21 @@ function Home(){
     return (
         <main>
             {/* info about website / wishlist */}
+            <h1>Christmas shopping doesn't need to look like this...</h1>
+
             <img src="https://i.giphy.com/3oz8xOuOFqmBi7c3IY.webp" alt="Chaotic Christmas"/>
-            <h3>Christmas shopping</h3>
+
+            <p>
+                That's why I put together this easy app where you can see my wishlist and 
+                order everything online! No more long lines or feeling like you're playing tackle football.
+
+                The app is easy to manuever. If you'd like to view gifts to purchase, you can view them under the Wishlist tab.
+                Once you purchase a gift, make sure to mark it as purchased in the app so no one buys duplicates! 
+
+                No need to worry about the Add New Gift tab, this is for me to add new gift ideas to my wishlist. 
+                Excited to celebrate with everyone! 
+                
+            </p>
 
 
         </main>
